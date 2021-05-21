e657975d01d698a7a5e09ffbc8f6618a0a8df960
Shruti Panjwani <67150258+shrutipanjwani@users.noreply.github.com>
2021-05-21 12:55:44 +0530

    api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db.js");
+const data = require("./data/people.json");
 
 dotenv.config();
 
@@ -8,11 +10,45 @@ const app = express();
 
 connectDB();
 
-app.get("/", (req, res) => {
-	res.send("API is running!");
-});
-
 const PORT = process.env.PORT || 5000;
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.use(cors());
+
+app.use(express.static(__dirname + "/public"));
+app.use("/images", express.static(__dirname + "/images"));
+
+app.get(
+	"/user/:id/",
+	(req, res, next) => {
+		// res.send(`get request is sending on port ${PORT}`);
+		console.log(req.params.id); // String
+		let user = Number(req.params.id); //Number/ Integer
+		console.log(user);
+		console.log(data[user]);
+		res.send(data[user]);
+		next();
+	},
+	(res, req) => {
+		console.log("The second function");
+	}
+);
+
+app
+	.route("/profiles")
+	.get((req, res) => {
+		// res.end();
+		// res.redirect("https://www.linkedin.com/");
+		res.json(data);
+	})
+	.put((req, res) => {
+		res.send(`put request is sending on port ${PORT}`);
+	})
+	.delete((req, res) => {
+		res.send(`delete request is sending on port ${PORT}`);
+	});
 
 app.listen(
 	PORT,
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,52 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-	return <div className="App">MERN Practices</div>;
+	const [profiles, setProfiles] = useState([]);
+
+	const getProfilesHere = () => {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				authorization: "Access-Control-Allow-Origin",
+			},
+		};
+		try {
+			axios.get("http://localhost:5000/profiles", config).then((res) => {
+				var profiles = res.data;
+				// console.log(profiles);
+				setProfiles(profiles);
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		getProfilesHere();
+	});
+
+	return (
+		<>
+			<div className="App">
+				<ul>
+					{profiles.length > 0 ? (
+						profiles.map((profile) => (
+							<li key={profile.id}>
+								<p>Myself {profile.name}</p>
+								<p>Contact me - {profile.email}</p>
+								<p>I am {profile.age} years old</p>
+								<p>I was born in {profile.YOB}</p>
+							</li>
+						))
+					) : (
+						<h4>No profiles found...</h4>
+					)}
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default App;
